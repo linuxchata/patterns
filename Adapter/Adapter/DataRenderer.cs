1f3e404d6280eabaa79b635ee0882d7f0f85714c
LinuxChata <linuxchata@hotmail.com>
2017-03-14 22:08:50 +0100

    Adapter: Implement pattern

---
+++ Adapter/Adapter/DataRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.IO;
+
+namespace Adapter
+{
+    public class DataRenderer
+    {
+        private readonly IDbDataAdapter adapter;
+
+        public DataRenderer(IDbDataAdapter adapter)
+        {
+            this.adapter = adapter;
+        }
+
+        public void Render(TextWriter writer)
+        {
+            var dataSet = new DataSet();
+
+            this.adapter.Fill(dataSet);
+
+            foreach (DataTable table in dataSet.Tables)
+            {
+                foreach (DataColumn column in table.Columns)
+                {
+                    writer.Write(column.ColumnName.PadRight(6) + " ");
+                }
+
+                writer.WriteLine();
+
+                foreach (DataRow row in table.Rows)
+                {
+                    for (var i = 0; i < table.Columns.Count; i++)
+                    {
+                        writer.Write(row[i].ToString().PadRight(6) + " ");
+                    }
+
+                    writer.WriteLine();
+                }
+
+                writer.WriteLine();
+            }
+        }
+    }
+}
--- Adapter/Adapter/Program.cs
+++ Adapter/Adapter/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlServerCe;
+using System.IO;
 
 namespace Adapter
 {
@@ -6,6 +9,30 @@ namespace Adapter
     {
         static void Main(string[] args)
         {
+            var adapter = new StubAdapter();
+
+            var renderer = new DataRenderer(adapter);
+            var stringWriter = new StringWriter();
+            renderer.Render(stringWriter);
+            Console.WriteLine(stringWriter.ToString());
+
+            var connection = new SqlCeConnection(@"Data Source=db.sdf");
+            var command = new SqlCeCommand("SELECT * FROM Pattern", connection);
+            var dataAdapter = new SqlCeDataAdapter(command);
+
+            renderer = new DataRenderer(dataAdapter);
+            stringWriter = new StringWriter();
+            renderer.Render(stringWriter);
+            Console.WriteLine(stringWriter.ToString());
+
+            var list = new List<Pattern>
+            {
+                new Pattern { Id = 1, Name = "Strategy" },
+                new Pattern { Id = 2, Name = "MVC" }
+            };
+            var patternRendered = new PatternRenderer();
+            Console.WriteLine(patternRendered.PatternsList(list));
+
             Console.ReadKey();
         }
     }
