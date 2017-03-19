dbfc43f2a41703c55789187b3510d46787c02997
LinuxChata <linuxchata@hotmail.com>
2017-03-19 21:50:29 +0100

    Facade: Add base project

---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Facade
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
