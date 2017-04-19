95018f2c34229f719bc9330094896de1cb5a299c
LinuxChata <linuxchata@hotmail.com>
2017-04-19 20:15:22 +0200

    Facade: Implement pattern

--- Facade/Facade/Program.cs
+++ Facade/Facade/Program.cs
@@ -2,10 +2,18 @@
 
 namespace Facade
 {
+    /// <summary>
+    /// Provide a unified interface to a set of interfaces in a subsystem.
+    /// Façade defines a higher-level interface that makes the subsystem easier to use.
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
         {
+            var facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+
             Console.ReadKey();
         }
     }
