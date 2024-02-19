134226674215cfc8d189fc846181ed7dd970e211
Pylyp Lebediev <linuxchata@gmail.com>
2024-02-19 08:53:56 +0100

    Add more description for Proxy pattern

--- StructuralPatterns/Proxy/Proxy/Program.cs
+++ StructuralPatterns/Proxy/Proxy/Program.cs
@@ -4,6 +4,10 @@ namespace Proxy
 {
     /// <summary>
     /// Provide a surrogate or placeholder for another object to control access to it.
+    /// Variants of the Proxy pattern
+    /// A remote proxy provides a connection to the object being replaced, which is in a different address space.
+    /// A virtual proxy performs the creation of the replaced object only when it is really needed.
+    /// A protection proxy checks if the client has the necessary access rights to complete the request.
     /// </summary>
     public class Program
     {
