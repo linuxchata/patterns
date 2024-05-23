e4eb04519d76418061b37239e06b15080673a20d
linuxchata <linuxchata@gmail.com>
2024-05-23 23:01:01 +0200

    Add Monostate pattern

---
+++ CreationalPatterns/Monostate/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace Monostate
+{
+    public class Person
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+    }
+}
---
+++ CreationalPatterns/Monostate/Program.cs
@@ -0,0 +1,8 @@
+﻿using Monostate;
+
+var person = new Person() { Name = "Oliver" };
+var anotherPerson = new Person();
+
+Console.WriteLine($"{person.Name} = {anotherPerson.Name}");
+
+Console.ReadKey();
